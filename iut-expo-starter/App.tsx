bee6f659baa4e4284a78c0f4f4b60da2aa72e95f
Arafamamadouellaphi <mamadouelaphi@gmail.com>
2023-06-10 15:48:54 +0200

    branche two

--- iut-expo-starter/App.tsx
+++ iut-expo-starter/App.tsx
@@ -1,14 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+ import { Search } from './Components/Search';
 
 export default function App() {
   return (
+   
     <View style={styles.container}>
+  <Search />
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+   
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
