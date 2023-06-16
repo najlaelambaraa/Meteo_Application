69cdc756b054e769a59d74b611975693db3a6e42
Arafamamadouellaphi <mamadouelaphi@gmail.com>
2023-06-16 10:08:14 +0200

    enfin

--- iut-expo-starter/Screen/MyList.tsx
+++ iut-expo-starter/Screen/MyList.tsx
@@ -1,13 +1,22 @@
 import { useNavigation } from '@react-navigation/native';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text, FlatList, TouchableOpacity, TouchableHighlight } from 'react-native';
 import { CITIES_DATA, City, WEATHER_DATA } from '../data/stub';
 import CityListItem from '../Components/CityList';
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import Search from '../Components/Search';
+import { GetListWeather } from '../redux/Action/GetList_Weater';
 
 export default function MyList({route,navigation}) {
   const cityList = useSelector(state => state.appReducer.city);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const loadCity = async () => {
+      await dispatch(GetListWeather());
+    };
+    loadCity();
+  }, [dispatch]);
 
   return (
     <View>
@@ -25,4 +34,4 @@ export default function MyList({route,navigation}) {
   );
 }
 
-export default MyList;
+;
